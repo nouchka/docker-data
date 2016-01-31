cc026994a0dcf5565b0766ac837ca781f3da1f32
nouchka <github@katagena.com>
2016-01-31 20:38:20 +0100

    simple backup

--- backup.sh
+++ backup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Backup"
+TMP_DIR=/tmp
+TMP_FILE=backup.tgz
+
+##Password file generation
+PASSWORD_FILE=/tmp/.rsync
+echo $RSYNC_PASSWORD > $PASSWORD_FILE
+chmod 400 $PASSWORD_FILE
+
+cd $DOCKER_DATA_DIRECTORY
+
+tar czPf $TMP_DIR/$TMP_FILE .
+
+rsync -azv --delete --password-file=$PASSWORD_FILE $TMP_DIR/$TMP_FILE $RSYNC_USER@$RSYNC_SERVER::$RSYNC_REPO/$RSYNC_FILE
